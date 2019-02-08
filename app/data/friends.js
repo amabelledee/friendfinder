f238447107133a7a146997981cd50263bd3cee93
Hatshepsut <amabelle.delacruz322@gmail.com>
2019-02-07 21:07:48 -0800

    added more code

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friends = [
+    {
+        name: "Mr. Poopybutthole",
+        photo: "",
+        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    },
+    {
+        name: "Mr. Poopybutthole",
+        photo: "",
+        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    },
+    {
+        name: "Mr. Poopybutthole",
+        photo: "",
+        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    },
+    {
+        name: "Mr. Poopybutthole",
+        photo: "",
+        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    },
+]
---
+++ app/routing/html-routes.js
@@ -0,0 +1,13 @@
+// creating routes
+var path = require ("path");
+
+module.exports = function (app) {
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/survey.html"));
+    });
+
+    //Makes a file default
+    app.use(function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/home.html"));
+    });
+};
