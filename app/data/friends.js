3420ff6b6c65fb85daf97d6b058ad2dc832ef7c0
Hatshepsut <amabelle.delacruz322@gmail.com>
2019-02-18 00:50:16 -0800

    fixed friend data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friends = [
     {
         name: "Mr. Poopybutthole",
-        photo: "",
-        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/3/37/Mr_poopy_butthole.png/revision/latest?cb=20150819161234",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     },
     {
-        name: "Mr. Poopybutthole",
-        photo: "",
-        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        name: "Jerry Smith",
+        photo: "https://misanthropester.files.wordpress.com/2015/10/tumblr_inline_n0op8nq0sb1qmqtm3.jpg",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     },
     {
-        name: "Mr. Poopybutthole",
-        photo: "",
-        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        name: "Unity",
+        photo: "https://hobbydb-production.s3.amazonaws.com/processed_uploads/subject_photo/subject_photo/image/37008/1518460561-29733-1441/unity-rick-and-morty-58.2.jpg",
+        scores: [4, 3, 4, 2, 3, 3, 5, 4, 1, 1]
     },
     {
-        name: "Mr. Poopybutthole",
-        photo: "",
-        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        name: "Dr. Wong",
+        photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/a4/Dr._Wong.png/revision/latest/scale-to-width-down/1000?cb=20170804214823",
+        scores: [4, 2, 1, 4, 5, 2, 3, 3, 4,5]
     },
 ]
